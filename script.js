34f8f638596c71033d3f91d1a24f5e1c34d43bdd
PrasannaKummari915 <www.prasannakummari9347@gmail.com>
2026-03-13 22:42:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+
+document.querySelector(".html").style.width="90%";
+document.querySelector(".css").style.width="85%";
+document.querySelector(".js").style.width="75%";
+document.querySelector(".php").style.width="70%";
+document.querySelector(".mysql").style.width="70%";
+document.querySelector(".java").style.width="80%";
+
+}
