b942f1bda3f0576518bb12d7aff181e5dc4626a2
PrasannaKummari915 <www.prasannakummari9347@gmail.com>
2026-03-13 20:17:21 +0530

    Add files via upload

---
+++ practice.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP Practice</title>
+</head>
+
+<body>
+
+<h1>PHP Example</h1>
+
+<?php
+$name = "Prasanna";
+
+echo "Welcome " . $name;
+?>
+
+</body>
+</html>
